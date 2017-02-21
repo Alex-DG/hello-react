ea93ba9919645cca47d98413631b9dd2ed45ea08
Alexandre Di Guida <alexandre.diguida@gmail.com>
2017-02-21 17:48:27 +0000

    Simple example showing how a react component works

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # hello-react
 
 Having fun with React !!
+
+*Working on Mac with Atom*
---
+++ public/app.jsx
@@ -0,0 +1,16 @@
+// Reusable React component!!
+var Greeter = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <h1>Hello React!</h1>
+        <p>This is form a component!</p>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Greeter/>,
+  document.getElementById('app')
+);
